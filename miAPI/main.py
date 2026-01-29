217dbfd6a74a8d498854abc34c4ea8a6b3c1e962
IvetMartinez <ivimar637@gmail.com>
2026-01-29 17:32:53 -0600

    Práctica 2 Avance

--- miAPI/main.py
+++ miAPI/main.py
@@ -1,15 +1,55 @@
+#Práctica 1
 #importaciones
+from typing import Optional
 from fastapi import FastAPI
+import asyncio   
 
 #Inicialización
-app= FastAPI()
+app= FastAPI(
+
+#Personalizar 
+title = 'Mi primer API',
+description='Ive Martinez',
+version='1.0'
+)
+#Práctica 2: Simulación de Base de datos
+
+usuarios=[
+    {"id":1,"nombre":"Ive", "edad":22},
+    {"id":2,"nombre":"Axel", "edad":35},
+    {"id":3,"nombre":"Ivi", "edad":20},
+]
+
 
 #Endpoints 
-@app.get("/")
-async def Holamundo():
+@app.get("/", tags=['Inicio'])
+async def holamundo():
     return {"mensaje":"Hola mundo FASTAPI"}
 
 
-@app.get("/bienvenidos")
-async def Bienvenidos():
+@app.get("/V1/bienvenidos", tags=['Inicio'])
+async def bienvenidos():
     return {"mensaje":"Bienvenido a tu API REST"}
+
+
+#Práctica 2
+
+#Se crea un nuevo endpoint
+@app.get("/v1/calificaciones", tags=['Asincronia'])
+async def calificaciones():
+    await asyncio.sleep(5) #No es obligatorio es una simulación de tiempo de espera 
+    return {"mensaje":"Tu calificación en TAI es 10"}
+
+
+#Definir parametros obloigatorios mediante las llaves
+
+@app.get("/v1/usuarios/{id}", tags=['Parametro obligatorio'])
+async def consultaUsuarios(id:int): 
+    return {"Usuario encontrado":id}
+
+@app.get("/v1/usuarios_op", tags=['Parametro opcional'])
+async def consultaOp(id:Optional[int]=None):
+    await asyncio.sleep(5)
+    if id is not None:
+        return {"Usuario encontrado":id}
+
