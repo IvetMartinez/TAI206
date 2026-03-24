98b15a35ba549a5f2df3e10eafac82173b12b59f
IvetMartinez <ivimar637@gmail.com>
2026-03-24 17:07:55 -0600

    P9

--- miAPI/app/main.py
+++ miAPI/app/main.py
@@ -1,7 +1,11 @@
 #Importaciones
 from fastapi import FastAPI
 from app.routers import usuarios, misc
+from app.data.db import engine
+from app.data import usuario 
 
+
+usuario.Base.metadata.create_all(bind=engine)
 
 #Inicializacion 
 app= FastAPI(
--- miAPI/app/models/usuario.py
+++ miAPI/app/models/usuario.py
@@ -2,8 +2,7 @@
 #Importaciones 
 from pydantic import BaseModel, Field
 
-class UauarioBase(BaseModel):
-    id:int = Field(...,gt=0,description="Identificador de usuario",example="1")
+class UsuarioBase(BaseModel):
     nombre: str = Field(...,min_length=3,max_length=50,description="Nombre del usuario")
     edad:int = Field(...,ge=0,le=121,description="Edad validada entre 0 y 121")
     
